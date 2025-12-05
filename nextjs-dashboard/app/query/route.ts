981e12025562726bb4adc9fc19e091f351b2936b
Bahaa Abbas <bahaaisl566@gmail.com>
2025-12-05 14:55:13 +0200

    a

--- nextjs-dashboard/app/query/route.ts
+++ nextjs-dashboard/app/query/route.ts
@@ -18,6 +18,7 @@ export async function GET() {
   //   message:
   //     "Uncomment this file and remove this line. You can delete this file when you are finished.",
   // });
+
   try {
     return Response.json(await listInvoices());
   } catch (error) {
